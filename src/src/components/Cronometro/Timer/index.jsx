05fa14340c9cfbede097be3280892a97d529eb98
Rogerio Silva <roger.alves.silva84@gmail.com>
2025-03-05 17:57:37 -0300

    Update

--- src/src/components/Cronometro/Timer/index.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./styles.module.css";
-
-export default function Timer() {
-  return <div className={styles["cronometer-timer"]}>30</div>;
-}
--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,34 @@
 import { create } from "zustand";
 
 export const MODO_CRONOMETRO = {
-    FOCO: {
-        id: "foco",
-        nome: "Foco",
-        frase: ["Otimize sua produtividade,", "mergulhe no que importa."],
-        tempoInicialEmSegundos: 30,
-    },
-    DESCANSO_CURTO: {
-        id: "descanso-curto",
-        nome: "Descanso curto",
-        frase: ["Que tal dar uma respirada?", "Faça uma pausa curta."],
-        tempoInicialEmSegundos: 5,
-    },
-    DESCANSO_LONGO: {
-        id: "descanso-longo",
-        nome: "Descanso longo",
-        frase: ["Hora de voltar à superfície.", "Faça uma pausa longa."],
-        tempoInicialEmSegundos: 15,
-    },
+  FOCO: {
+    id: "foco",
+    nome: "Foco",
+    frase: ["Otimize sua produtividade,", "mergulhe no que importa."],
+    tempoInicialEmSegundos: 30,
+  },
+  DESCANSO_CURTO: {
+    id: "descanso-curto",
+    nome: "Descanso curto",
+    frase: ["Que tal dar uma respirada?", "Faça uma pausa curta."],
+    tempoInicialEmSegundos: 5,
+  },
+  DESCANSO_LONGO: {
+    id: "descanso-longo",
+    nome: "Descanso longo",
+    frase: ["Hora de voltar à superfície.", "Faça uma pausa longa."],
+    tempoInicialEmSegundos: 15,
+  },
 };
 
 export const useCronometroStore = create((set) => ({
-    modoCronometro: MODO_CRONOMETRO.FOCO,
-    tempoEmSegundos: MODO_CRONOMETRO.FOCO.tempoInicialEmSegundos,
+  modoCronometro: MODO_CRONOMETRO.FOCO,
+  tempoEmSegundos: MODO_CRONOMETRO.FOCO.tempoInicialEmSegundos,
 
-    setModoCronometro: (novoModo) => {
-        set({
-            modoCronometro: novoModo,
-            tempoEmSegundos: novoModo.tempoInicialEmSegundos,
-        });
-    },
+  setModoCronometro: (novoModo) => {
+    set({
+      modoCronometro: novoModo,
+      tempoEmSegundos: novoModo.tempoInicialEmSegundos,
+    });
+  },
 }));
