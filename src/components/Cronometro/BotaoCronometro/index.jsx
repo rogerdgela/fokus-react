b85a61197f5d876109b20a0b7df2c513820d105f
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-03-17 17:43:45 -0300

    Criando o início do cronômetro e os estados do botão no componente

--- src/components/Cronometro/BotaoCronometro/index.jsx
+++ src/components/Cronometro/BotaoCronometro/index.jsx
@@ -3,14 +3,22 @@ import play_arrowImg from "/src/assets/imgs/play_arrow.png";
 import pauseImg from "/src/assets/imgs/pause.png";
 import audioPlaySom from "/src/assets/sons/play.wav";
 import audioPauseSom from "/src/assets/sons/pause.mp3";
-
+import { useCronometroStore } from "../../../store";
 
 export default function BotaoCronometro() {
+  const intervaloId = useCronometroStore((estado) => estado.intervaloId);
+  const iniciarCronometro = useCronometroStore((estado) => estado.iniciarCronometro);
+
+  const textoIniciarOuPausar = intervaloId ? "Pausar" : "Iniciar";
+  const iconeIniciarOuPausar = intervaloId ? pauseImg : play_arrowImg;
+
   return (
     <div className={styles["cronometer__primary-button-wrapper"]}>
-      <button className={styles["cronometer__primary-button"]}>
-        <img className={styles["cronometer__primary-button-icon"]} src={play_arrowImg} alt="" />
-        <span>Começar</span>
+      <button className={styles["cronometer__primary-button"]}
+        onClick={iniciarCronometro}
+      >
+        <img className={styles["cronometer__primary-button-icon"]} src={iconeIniciarOuPausar} alt="" />
+        <span>{textoIniciarOuPausar}</span>
       </button>
     </div>
   );
